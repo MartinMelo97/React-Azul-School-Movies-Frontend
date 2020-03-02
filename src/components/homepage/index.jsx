eb828c741230b76422be1f65b1dc7d6800dfc281
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2020-03-02 09:31:01 -0600

    Logo added

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.scss';
+import logo from '../../assets/logo.png'
 
 const Homepage = () => (
     <>
         <div className="welcome-container">
+            <img src={logo} alt="AzulSchoolLogo"/>
             <p className="welcome-text">
                 Bienvenido a la plataforma de películas más grande
                 de toda Azul School
